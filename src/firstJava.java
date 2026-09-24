e1fa742b5a540805cdc61b914da9b01d314b5923
Yuan <yapchiongcoyuan@gmail.com>
2026-09-24 08:54:10 +0800

    Remove unnecessary files from repository

--- src/firstJava.java
+++
@@ -1,6 +0,0 @@
-public class firstJava {
-    public static void main(String[] args){
-        System.out.println("Name: Manuel Francis Yuan Y. Sia \n" +
-                "section: BSIT 1A");
-    }
-}
